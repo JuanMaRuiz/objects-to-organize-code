8d61eb19412ae1748cc3d06e1bd31bea8cb9d490
JuanMa Ruiz <ruizjuanma@gmail.com>
2015-08-21 13:58:02 +0200

    Basic structure

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var myFeature = {
+	'config': {
+		'container': $('#myFeature')
+	},
+	'init': function() {},
+	'buildSectionNav': function() {},
+	'buildItemNav': function() {},
+	'showSection': function() {},
+	'showContentItem': function() {}
+}
